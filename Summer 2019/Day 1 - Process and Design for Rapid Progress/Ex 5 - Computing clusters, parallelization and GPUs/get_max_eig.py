97b9fd257efc1065b519b415b996d8de4ab7c67e
Jojk <gimbutas@gmail.com>
2019-07-17 18:05:53 -0500

    corrected get eigs function

--- Summer 2019/Day 1 - Process and Design for Rapid Progress/Ex 5 - Computing clusters, parallelization and GPUs/get_max_eig.py
+++ Summer 2019/Day 1 - Process and Design for Rapid Progress/Ex 5 - Computing clusters, parallelization and GPUs/get_max_eig.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 def get_max_eig(chunk):
   idx=chunk[1] 
   arry=chunk[0]
   eigvals=np.linalg.eigvalsh(arry)
-  joint_output.put((eigvals.max(),idx))
+  #joint_output.put((eigvals.max(),idx))
   return (eigvals.max(),idx)
