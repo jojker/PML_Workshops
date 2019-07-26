60866c0b55ac9a9b789f5a0a78e279d6aac37a83
Jojk <gimbutas@gmail.com>
2019-07-25 19:33:42 -0500

    deleted extra files from 2017 branch

--- Summer 2019/Day 1 - Process and Design for Rapid Progress/Ex 5 - Computing clusters, parallelization and GPUs/get_max_eig.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def get_max_eig(chunk):
-  idx=chunk[1] 
-  arry=chunk[0]
-  eigvals=np.linalg.eigvalsh(arry)
-  #joint_output.put((eigvals.max(),idx))
-  return (eigvals.max(),idx)
